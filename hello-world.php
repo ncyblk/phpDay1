5968b30a5845252d0928bf11250ebe922bfcfda2
ncyblk <82343905+ncyblk@users.noreply.github.com>
2021-04-19 16:41:45 +0200

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang= "en">
+   <head>
+        <meta charset="UTF-8">
+       <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+       <title>Hello World</title>
+
+       <link rel="stylesheethelloworld" href="stylesheethelloworld.css";
+     
+
+   <body>
+        <?php echo "<h1>Hello, World!</h1>", 
+        "<h2>jetzt nochnal hallo</h2>"
+
+        ?>
+   </body>
+</html>
